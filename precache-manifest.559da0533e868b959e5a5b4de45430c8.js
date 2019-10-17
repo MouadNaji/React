41a11a58e54551c3a9e0f0f3bea08e17e0c56c92
Mouad Naji <mouadnaji@Mouads-MacBook-Pro.local>
2019-10-17 10:36:39 +0200

    Updates

---
+++ precache-manifest.559da0533e868b959e5a5b4de45430c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f4be47a7b42ebaac47eb51f0f062b0",
+    "url": "/reponame/index.html"
+  },
+  {
+    "revision": "b8bb3f660df548dcad28",
+    "url": "/reponame/static/css/2.5158c8ec.chunk.css"
+  },
+  {
+    "revision": "2472c2c0b2657be84ad4",
+    "url": "/reponame/static/css/main.b76d40d3.chunk.css"
+  },
+  {
+    "revision": "b8bb3f660df548dcad28",
+    "url": "/reponame/static/js/2.b28d496a.chunk.js"
+  },
+  {
+    "revision": "2472c2c0b2657be84ad4",
+    "url": "/reponame/static/js/main.1d90cb2c.chunk.js"
+  },
+  {
+    "revision": "72d5f9aea4861539d5ce",
+    "url": "/reponame/static/js/runtime-main.0eb1c1eb.js"
+  }
+]);
